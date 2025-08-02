a49fcb313385ebeadce59c0fad8951b33ffbb56b
Trillion Digital <marketing@trilliondigital.net>
2025-08-02 18:36:33 -0500

    Xcode project restructure

---
+++ Dirt/Dirt/DirtApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirtApp.swift
+//  Dirt
+//
+//  Created by Kaegan Braud on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct DirtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
